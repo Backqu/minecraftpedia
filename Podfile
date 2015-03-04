178a5a44e4f6ec8253e419b006442a8118047209
Lucas Juillard <lucasjuju@hotmail.fr>
2015-03-04 16:03:12 +0100

    - ajout des image sur le jSON -supression des categorie

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '8.0'
+
+target 'minecraftpedia' do
+    pod "AFNetworking"
+end
+
+target 'minecraftpediaTests' do
+
+end
+
--- minecraftpedia/ItensViewController.swift
+++ minecraftpedia/ItensViewController.swift
@@ -9,50 +9,46 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
+    var items: [JSON]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		
-		//load json
-		
+
 		let path = NSBundle.mainBundle().pathForResource("minecraft", ofType: "json")
 		let data = NSData(contentsOfFile: path!)
-		let json = JSON(data: data!)
-		if let itemID = json["minecraft"]["daylight_detector"]["itemID"].int {
-			println("\(itemID)")
-		}
-		
-		for (key: String, subJson: JSON) in json["minecraft"] {
-			println(subJson["name"])
-		}
-		
-		
-    }
-    
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 3
-    }
-    
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch (section) {
-        case 0:
-            return "Tools"
-        case 1:
-            return "Blocks"
-        case 2:
-            return "Weapons"
-        default:
-            assertionFailure("error")
+        self.items = JSON(data: data!)["minecraft"].dictionaryValue.values.array.sorted { (a, b) in
+            return a["name"] < b["name"]
         }
     }
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+//    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        switch (section) {
+//        case 0:
+//            return "Tools"
+//        case 1:
+//            return "Blocks"
+//        case 2:
+//            return "Weapons"
+//        default:
+//            assertionFailure("error")
+//        }
+//    }
+    
     override func  tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.items.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
-        cell.textLabel!.text = "Item"
+        
+        let item = self.items[indexPath.row]
+        cell.textLabel!.text = item["name"].stringValue
+        let image = item["image"].stringValue
+        cell.imageView!.setImageWithURL(NSURL(string: "http://assets.wurstmineberg.de/img/grid/\(image)"))
         return cell
     }
 }							
